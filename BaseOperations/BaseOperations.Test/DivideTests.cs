d2289d1cdb0650647b1e024fdc44f6bf4d48b90b
Axel <tranquilized.mind@gmail.com>
2013-06-05 14:25:19 +0500

    Операция /

---
+++ BaseOperations/BaseOperations.Test/DivideTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace BaseOperations.Test
+{
+    public class DivideTests
+    {
+        Divide divide = new Divide();
+
+        [Fact]
+        public void Operation_PerformsCorrectly()
+        {
+            var result = divide.Perform(new double[] { 36, 36 });
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Throws_DivideByZeroException()
+        {
+            Assert.Throws<DivideByZeroException>(() => divide.Perform(new double[] {36, 0}));
+        }
+    }
+}
---
+++ BaseOperations/BaseOperations/Divide.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Domain;
+
+namespace BaseOperations
+{
+    public class Divide:IOperation
+    {
+        public double Perform(params double[] arguments)
+        {
+            var result = arguments[0]/arguments[1];
+
+            if (!double.IsInfinity(result))
+            {
+                return result;
+            }
+            throw new DivideByZeroException("Попытка деления на ноль");
+        }
+
+        public int Arity
+        {
+            get { return 2; }
+        }
+
+        public int Priority
+        {
+            get { return 3; }
+        }
+
+        public string StringPresentation
+        {
+            get { return "/"; }
+        }
+    }
+}
