c6514cc4bcd011a67689629dc36bb41c13f5ca42
Axel <tranquilized.mind@gmail.com>
2013-06-05 13:50:40 +0500

    Реализация операции +

--- BaseOperations/BaseOperations/Plus.cs
+++ BaseOperations/BaseOperations/Plus.cs
@@ -1,7 +1,28 @@
-﻿namespace BaseOperations
+﻿using System.Linq;
+using Domain;
+
+namespace BaseOperations
 {
-    public class Plus
+    public class Plus:IOperation
     {
-         
+        public double Perform(params double[] arguments)
+        {
+            return arguments.Sum();
+        }
+
+        public int Arity
+        {
+            get { return 2; }
+        }
+
+        public int Priority
+        {
+            get { return 4; }
+        }
+
+        public string StringPresentation
+        {
+            get { return "+"; }
+        }
     }
 }
