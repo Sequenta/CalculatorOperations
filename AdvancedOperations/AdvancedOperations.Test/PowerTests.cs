3daa707b100c2c35e222735da383a7b5275117c3
Axel <tranquilized.mind@gmail.com>
2013-06-05 18:46:31 +0500

    Операция возведения в степень

---
+++ AdvancedOperations/AdvancedOperations.Test/PowerTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace AdvancedOperations.Test
+{
+    public class PowerTests
+    {
+        private Power power = new Power();
+
+        [Fact]
+        public void Operation_PerformsCorrectly()
+        {
+            var result = power.Perform(new double[] {2,3});
+
+            Assert.Equal(8, result);
+        }  
+    }
+}
---
+++ AdvancedOperations/AdvancedOperations/Power.cs
@@ -0,0 +1,33 @@
+﻿using Domain;
+
+namespace AdvancedOperations
+{
+    public class Power:IOperation
+    {
+        public double Perform(params double[] arguments)
+        {
+            var power = arguments[1];
+            var result = arguments[0];
+            for (var i = 1; i < power; i++)
+            {
+                result *= arguments[0];
+            }
+            return result;
+        }
+
+        public int Arity
+        {
+            get { return 2; }
+        }
+
+        public int Priority
+        {
+            get { return 2; }
+        }
+
+        public string StringPresentation
+        {
+            get { return "^"; }
+        }
+    }
+}
