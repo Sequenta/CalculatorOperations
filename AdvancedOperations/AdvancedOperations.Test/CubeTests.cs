10b2bd4efdf6da62f099fb123479af650ee883e1
Axel <tranquilized.mind@gmail.com>
2013-06-05 18:46:11 +0500

    Операция возведения в куб

---
+++ AdvancedOperations/AdvancedOperations.Test/CubeTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace AdvancedOperations.Test
+{
+    public class CubeTests
+    {
+        private Cube cube = new Cube();
+
+        [Fact]
+        public void Operation_PerformsCorrectly()
+        {
+            var result = cube.Perform(new double[] { 4 });
+
+            Assert.Equal(64, result);
+        } 
+    }
+}
