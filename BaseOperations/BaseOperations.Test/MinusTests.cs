91617a7c440fc4007dd92f74c09afc0fbff879ea
Axel <tranquilized.mind@gmail.com>
2013-06-05 14:02:10 +0500

    Операция -

---
+++ BaseOperations/BaseOperations.Test/MinusTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace BaseOperations.Test
+{
+    public class MinusTests
+    {
+        [Fact]
+        public void Operation_PerformsCorrectly()
+        {
+            var minus = new Minus();
+
+            var result = minus.Perform(new double[] {2, 2});
+
+            Assert.Equal(0,result);
+        } 
+    }
+}
---
+++ BaseOperations/BaseOperations/Minus.cs
@@ -0,0 +1,25 @@
+﻿using Domain;
+
+namespace BaseOperations
+{
+    public class Minus:IOperation
+    {
+        public double Perform(params double[] arguments)
+        {
+            return arguments[0] - arguments[1];
+        }
+
+        public int Arity
+        {
+            get { return 2; }
+        }
+        public int Priority
+        {
+            get { return 4; }
+        }
+        public string StringPresentation
+        {
+            get { return "-"; }
+        }
+    }
+}
