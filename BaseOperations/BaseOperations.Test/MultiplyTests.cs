926059a86aa8b79afe528327ba25120a0d586d58
Axel <tranquilized.mind@gmail.com>
2013-06-05 14:18:41 +0500

    Операция *

---
+++ BaseOperations/BaseOperations.Test/MultiplyTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace BaseOperations.Test
+{
+    public class MultiplyTests
+    {
+        Multiply multiply = new Multiply();
+
+        [Fact]
+        public void Operation_PerformsCorrectly()
+        {
+            var result = multiply.Perform(new double[] {6, 6});
+
+            Assert.Equal(36,result);
+        }
+
+        [Fact]
+        public void Throws_OverflowException()
+        {
+            Assert.Throws<OverflowException>(() => multiply.Perform(new[] { double.MaxValue, double.MaxValue }));
+        }
+    }
+}
---
+++ BaseOperations/BaseOperations/Multiply.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Domain;
+
+namespace BaseOperations
+{
+    public class Multiply:IOperation
+    {
+        public double Perform(params double[] arguments)
+        {
+            var result = arguments[0] * arguments[1];
+            if(!double.IsInfinity(result))
+            {
+                return result;
+            }
+            throw new OverflowException("Слишком большие числа");
+        }
+
+        public int Arity
+        {
+            get { return 2; }
+        }
+
+        public int Priority
+        {
+            get { return 3; }
+        }
+
+        public string StringPresentation
+        {
+            get { return "*"; }
+        }
+    }
+}
