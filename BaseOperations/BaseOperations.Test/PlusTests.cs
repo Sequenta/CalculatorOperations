92806a9a9fc34718b72b96b9648aa0054caec640
Axel <tranquilized.mind@gmail.com>
2013-06-05 14:01:55 +0500

    Тест для операции +

---
+++ BaseOperations/BaseOperations.Test/PlusTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace BaseOperations.Test
+{
+    public class PlusTests
+    {
+        [Fact]
+        public void Operation_PerformsCorrectly()
+        {
+            var plus = new Plus();
+
+            var result = plus.Perform(new double[] {2, 2});
+
+            Assert.Equal(4,result);
+        }
+    }
+}
