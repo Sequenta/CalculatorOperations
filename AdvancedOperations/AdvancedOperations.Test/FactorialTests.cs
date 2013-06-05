df20b07bbf6b2e317819bcaff9dd9bc0f4db07ec
Axel <tranquilized.mind@gmail.com>
2013-06-05 18:12:00 +0500

    Операция факториал

---
+++ AdvancedOperations/AdvancedOperations.Test/FactorialTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace AdvancedOperations.Test
+{
+    public class FactorialTests
+    {
+        private Factorial factorial = new Factorial();
+
+        [Fact]
+        public void Operation_PerformsCorrectly()
+        {
+            var result = factorial.Perform(new double[] {4});
+
+            Assert.Equal(24, result);
+        }
+    }
+}
---
+++ AdvancedOperations/AdvancedOperations/Factorial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Domain;
+
+namespace AdvancedOperations
+{
+    public class Factorial:IOperation
+    {
+        public double Perform(params double[] arguments)
+        {
+            var result = GetFactorial(arguments[0]);
+            if (!double.IsInfinity(result))
+            {
+                return result;
+            }
+            throw new OverflowException("Результат факториала слишком большой");
+        }
+
+        private double GetFactorial(double number)
+        {
+            if (number == 0 || number == 1)
+            {
+                return 1;
+            }
+            return number * GetFactorial(number - 1);
+        }
+
+        public int Arity
+        {
+            get { return 1; }
+        }
+
+        public int Priority
+        {
+            get { return 2; }
+        }
+        public string StringPresentation
+        {
+            get { return "fact"; }
+        }
+    }
+}
